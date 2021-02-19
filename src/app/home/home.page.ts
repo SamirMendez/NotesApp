03b17012f18cbd150d058379cfcb451e729a9ba8
samiMendez <samimendez@hotmail.es>
2021-02-19 19:49:07 -0400

    En este commit se hizo el homepage y se conecto con el login y el register

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController, ToastController } from '@ionic/angular';
+import { LoginPage } from '../core/authentication/login/login.page';
+import { RegisterPage } from '../core/authentication/register/register.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router, private nativeToast: ToastController, private nativeModal: ModalController) {}
 
+
+  // Funcion para mostrar el modal de login
+  async showLogin(): Promise<any> {
+    const loginModal = await this.nativeModal.create({
+      component: LoginPage,
+      animated: true,
+      swipeToClose: true,
+      showBackdrop: true,
+      cssClass: 'loginModal'
+    });
+    loginModal.present();
+  }
+  // Funcion para mostrar el modal de login
+  // Funcion para mostrar el modal de registro
+  async showRegister(): Promise<any> {
+    const registerModal = await this.nativeModal.create({
+      component: RegisterPage,
+      animated: true,
+      swipeToClose: true,
+      showBackdrop: true,
+      cssClass: 'registerModal'
+    });
+    registerModal.present();
+  }
+  // Funcion para mostrar el modal de registro
 }
